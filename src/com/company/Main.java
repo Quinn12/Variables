89130b12090c0cf56506a5151673182b1fcfe88b
Quinn12 <qhastings@students.mchenry.edu>
2018-09-04 20:43:01 -0500

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        byte Byte = 10;
+        System.out.println("A byte is integer that holds only 8-bits. Byte = " + Byte);
+        short Short = 1;
+        System.out.println("A short is integer that holds only 16-bits. Short = " + Short);
+	    int Int = 9;
+	    System.out.println("A int or integer holds 32-bits. Int = " + Int);
+        long Long = 9000000;
+        System.out.println("A Long is a longer integer that holds 64-bits. Long = " + Long);
+	    float Float = 0.6f;
+        System.out.println("A Float is a floating point or a one decimal number. Float = " + Float);
+        double Double = 0.62475679234;
+        System.out.println("A double is a floating point or a one decimal number that holds 64-bits. Double = " + Double);
+        boolean Bool = false;
+        System.out.println("A Boolean is a true or false variable. Bool = " + Bool);
+        char Char = 'w';
+        System.out.println("A Char is a character, not a string. Char = " + Char);
+
+
+    }
+}
